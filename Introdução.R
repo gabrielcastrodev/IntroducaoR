3523f48e58f83b073fbae103d41ab72e59bbde8c
gabrielcastrodev <gcastrop5@gmail.com>
2018-08-24 23:16:39 -0300

    First Commit

---
+++ Introdução.R
@@ -0,0 +1,24 @@
+# Curso Introdução à R DataCamp
+
+# Atribuição de valores à variáveis
+my_apples <- 5;
+print(my_apples);
+
+# Utilização de funções matemáticas com variáveis
+my_oranges <- 6;
+my_fruit <- my_apples + my_oranges;
+
+# Função print
+print(my_fruit);
+
+# Função numérica ou Float ou Double em outras linguagens
+my_numeric <- 42.22;
+
+# Função de String
+my_char <- "Universe";
+
+# Função Booleana
+my_boolean <- TRUE;
+
+# Check no tipo de variável
+class(my_boolean)
