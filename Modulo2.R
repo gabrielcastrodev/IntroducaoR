2ee3822a7ad85b24af0f1a93b1323c3b0e03b161
gabrielcastrodev <gcastrop5@gmail.com>
2018-08-24 23:18:25 -0300

    Modulo 2

---
+++ Modulo2.R
@@ -0,0 +1,12 @@
+# Curso Introdução à R DataCamp Módulo 2
+# Vetores
+
+# Para adicionar valores em vetor em uma variável se usa o "c()"
+# Vetor numerico
+vector_numeric <- c(1, 2, 3);
+
+# Vetor de caractere
+vector_char <- c("Matheus", "Gabriel", "Marcos");
+
+# Vetor lógico
+vector_logic <- c(TRUE, FALSE, TRUE);
