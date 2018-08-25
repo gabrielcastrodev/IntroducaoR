90b345cfddd38a11611ee0f305068087265be03a
gabrielcastrodev <gcastrop5@gmail.com>
2018-08-25 02:06:54 -0300

    Modulo 3 Meio

--- Modulo3.R
+++ Modulo3.R
@@ -19,3 +19,35 @@ variavel_box <- c(primeiro_vetor, segundo_vetor, terceiro_vetor)
 # Agora criamos a matrix com a nossa Variavel Box
 matrix_vetores <- matrix(variavel_box, byrow = TRUE, nrow = 3)
 print(matrix_vetores)
+
+# Dando nomes às linhas e colunas da nossa matrix
+# Criando vetores com titulos de colunas e de linha
+# COLNAMES(nome_matrix) <- vetor_nomes_colunas = Para adicionar um nome à coluna
+# ROWNAMES(nome_matrix) <- vetor_nome_linhas = Para adicionar um nome às linhas
+colunas = c("Coluna1", "Coluna2")
+linhas = c("Linha1", "Linha2", "Linha3")
+colnames(matrix_vetores) <- colunas
+rownames(matrix_vetores) <- linhas
+print(matrix_vetores)
+
+# FUnção rowSums() = Soma os valores de linha da matrix
+soma_linhas_matrix <- rowSums(matrix_vetores)
+
+# Como a função rowSums() cria um vetor, nós precisamos adicionar este vetor para a nossa matrix
+# Utilizamos a função cbind() para mesclarmos o nosso vetor com nossa matrix
+mescla_vetor_matrix = cbind(soma_linhas_matrix, matrix_vetores)
+print(mescla_vetor_matrix)
+
+# A função cbind() é utilizada apenas para a junção de vetores com matrizes
+# Para merclarmos matrizes com matrizes utilizamos a função rbind()
+total_valores_vetores <- c(589.15, 155.56, 189.696, 156.25, 789.358, 587.96)
+matrix_vetores2 <- matrix(total_valores_vetores, byrow = TRUE, nrow = 3)
+linhas_matrix2 <- c("Linha4", "Linha5", "Linha6")
+rownames(matrix_vetores2) <- linhas_matrix2
+# Uso do rbind()
+mescla_matrizes <- rbind(matrix_vetores, matrix_vetores2)
+print(mescla_matrizes)
+
+# Assim como temos o rowSums() para somar linhas, nós temos o colSums() para somar as colunas
+soma_colunas <- colSums(mescla_matrizes)
+print(soma_colunas)
